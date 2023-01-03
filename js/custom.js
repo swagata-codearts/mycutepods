0767b20fdcc6ae02143305e5a7a4f4c1efcca7f3
swagata-codearts <swagata.codearts@gmail.com>
2023-01-03 11:52:53 +0530

    Initials files added

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+jQuery('#custom-owl').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+jQuery(document).ready(function(){
+    jQuery(window).scroll(function() {
+       if(jQuery(this).scrollTop() > 200 ) {
+       jQuery(".custom-header").addClass("fixed-top");
+       jQuery(".header-top").slideUp();
+      } else {
+       jQuery(".custom-header").removeClass("fixed-top");
+       jQuery(".header-top").slideDown();
+      }
+    });
+});
